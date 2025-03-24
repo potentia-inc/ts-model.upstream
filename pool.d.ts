70b39eece351a2e3b582c174f02653339b954ee3
Tz-Huan Huang <tzhuan@potentia.asia>
2025-03-24 16:42:19 +0800

    Release 2.0.0

--- pool.d.ts
+++ pool.d.ts
@@ -6,16 +6,14 @@ type Hint = {
 type PoolOptions = {
     ttl: number;
     minFailures: number;
-    cooldown: number;
-    minCooldown: number;
-    maxCooldown: number;
+    minWeight: number;
+    decay: number;
 };
 export declare class Pool {
     #private;
     constructor(upstreams: Upstreams, type: string, options?: Partial<PoolOptions>);
     sample(hint?: Hint): Promise<Upstream>;
-    succeed(upstream: UpstreamOrId): void;
-    fail(upstream: UpstreamOrId): void;
-    debug(): void;
+    succeed(id: UpstreamOrId): void;
+    fail(id: UpstreamOrId): void;
 }
 export {};
--- pool.js
+++ pool.js
@@ -3,6 +3,7 @@ import { debug } from 'node:util';
 import { NoUpstreamError } from './error.js';
 import { pickId, pickIdOrNil } from './model.js';
 import { isNullish } from './type.js';
+import { Upstream } from './upstream.js';
 import { sleep } from './util.js';
 const DEBUG = debug('potentia:model:upstream');
 const DEBUG_VERBOSE = debug('potentia:model:upstream:verbose');
@@ -13,6 +14,7 @@ export class Pool {
     #caches = [];
     #failures = new Map();
     #times = new Map();
+    #weights = new Map();
     #expiresAt = 0;
     constructor(upstreams, type, options = {}) {
         this.#upstreams = upstreams;
@@ -20,16 +22,14 @@ export class Pool {
         this.#options = {
             ttl: options.ttl ?? 60,
             minFailures: options.minFailures ?? 0,
-            cooldown: options.cooldown ?? 10,
-            minCooldown: options.minCooldown ?? 0,
-            maxCooldown: options.maxCooldown ?? 86400,
+            minWeight: options.minWeight ?? 0.01,
+            decay: options.decay ?? 0.8,
         };
         assert(this.#options.ttl >= 1 &&
             this.#options.minFailures >= 0 &&
-            this.#options.cooldown >= 0 &&
-            this.#options.minCooldown >= 0 &&
-            this.#options.maxCooldown >= 0 &&
-            this.#options.maxCooldown >= this.#options.minCooldown);
+            this.#options.minWeight >= 0 &&
+            this.#options.decay > 0 &&
+            this.#options.decay < 1);
     }
     async sample(hint) {
         await this.#sync();
@@ -48,39 +48,43 @@ export class Pool {
             throw new NoUpstreamError();
         // weighted sample a upstream
         const upstream = (() => {
-            const sum = candidates.reduce((s, x) => s + x.weight, 0);
+            const sum = candidates.reduce((s, x) => s + this.#weight(this.#key(x), x.weight), 0);
             let rand = Math.random() * sum;
             for (const x of candidates)
-                if ((rand -= x.weight) <= 0)
+                if ((rand -= this.#weight(this.#key(x), x.weight)) <= 0)
                     return x;
             throw new NoUpstreamError(); // should not reach here!
         })();
-        // wait awhile if necessary
+        // wait a while if necessary
         const key = this.#key(upstream);
-        const cooldown = this.#cooldown(upstream) * 1000;
-        const duration = (this.#times.get(key) ?? 0) + cooldown - Date.now();
+        const duration = this.#time(key) + upstream.interval * 1000 - Date.now();
         if (duration > 0)
             await sleep(duration);
         this.#times.set(key, Date.now());
         DEBUG(`sample: ${candidates.length} ${key}`);
         return upstream;
     }
-    succeed(upstream) {
+    succeed(id) {
+        const upstream = id instanceof Upstream ? id : this.#caches.find((x) => x.id.equals(id));
+        assert(!isNullish(upstream));
         const key = this.#key(upstream);
         DEBUG(`succeed:${key}`);
         this.#failures.set(key, 0);
+        this.#weights.set(key, upstream.weight);
     }
-    fail(upstream) {
+    fail(id) {
+        const upstream = id instanceof Upstream ? id : this.#caches.find((x) => x.id.equals(id));
+        assert(!isNullish(upstream));
         const key = this.#key(upstream);
-        DEBUG(`fail:${key}`);
-        this.#failures.set(key, (this.#failures.get(key) ?? 0) + 1);
-    }
-    debug() {
-        const now = new Date();
-        console.error(now.toISOString(), now.getTime(), this.#type, this.#options, this.#expiresAt);
-        for (const x of this.#caches) {
-            const key = this.#key(x);
-            console.error(key, this.#failures.get(key) ?? 0, this.#times.get(key) ?? 0, this.#cooldown(x));
+        const failure = this.#failure(key) + 1;
+        this.#failures.set(key, failure);
+        if (failure >= this.#options.minFailures) {
+            const weight = this.#weight(key, upstream.weight) * this.#options.decay;
+            DEBUG(`fail:${key}: ${failure} ${weight}`);
+            this.#weights.set(key, weight);
+        }
+        else {
+            DEBUG(`fail:${key}: ${failure} ${this.#weight(key, upstream.weight)}`);
         }
     }
     async #sync() {
@@ -112,23 +116,23 @@ export class Pool {
             const key = this.#key(x);
             return {
                 key,
-                weight: x.weight,
-                failures: this.#failures.get(key) ?? 0,
-                cooldown: this.#cooldown(x),
+                failure: this.#failure(key),
+                time: this.#time(key),
+                weight: this.#weight(key, x.weight),
             };
         })));
     }
     #key(upstream) {
         return String(pickId(upstream));
     }
-    #cooldown(upstream) {
-        const key = this.#key(upstream);
-        const failures = this.#failures.get(key) ?? 0;
-        if (failures === 0 || failures < this.#options.minFailures) {
-            return upstream.interval;
-        }
-        const cooldown = Math.max(this.#options.minCooldown, Math.min(failures * this.#options.cooldown, this.#options.maxCooldown));
-        return Math.max(upstream.interval, cooldown);
+    #failure(key) {
+        return this.#failures.get(key) ?? 0;
+    }
+    #time(key) {
+        return this.#times.get(key) ?? 0;
+    }
+    #weight(key, weight) {
+        return this.#weights.get(key) ?? weight;
     }
 }
 //# sourceMappingURL=pool.js.map
